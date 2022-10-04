289ebcdbadd538815de3dd63e46371893750bfde
Zhai <1978235424@qq.com>
2022-10-04 23:05:21 +0800

    third idea

--- src/main/java/com/z/test.java
+++ src/main/java/com/z/test.java
@@ -5,6 +5,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("bbbbb");
+        System.out.println("ccccc");
     }
 
 }

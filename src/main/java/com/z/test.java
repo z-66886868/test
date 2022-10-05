f8cf4cebeaa6dd54284406d7eb98bfadbc58855f
Zhai <1978235424@qq.com>
2022-10-05 16:37:00 +0800

    hot-fix first commit

--- src/main/java/com/z/test.java
+++ src/main/java/com/z/test.java
@@ -1,11 +1,21 @@
 package com.z;
 
+/**
+* description: TODD
+* @author Fly
+* @date 2022/10/5 16:36
+* @version 1.0
+*/
 public class test {
 
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("bbbbb");
         System.out.println("ccccc");
+    }
+
+    public String string(String s){
+        return s;
     }
 
 }
